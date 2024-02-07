c1d6fba9c2d6e9d247c2bb98a27b7b2b41f31ff2
Ian Ramos <102131182+ianram0s@users.noreply.github.com>
2024-02-07 05:34:46 -0300

    Initial commit

---
+++ web/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { CONFIG, IS_BROWSER } from './stores/stores';
+    import { InitialiseListen } from '@utils/listeners';
+    import Visibility from '@providers/Visibility.svelte';
+    import ImageHolder from '@components/ImageHolder.svelte';
+
+    CONFIG.set({
+        fallbackResourceName: 'debug',
+        allowEscapeKey: true,
+    });
+
+    InitialiseListen();
+</script>
+
+<Visibility>
+    <ImageHolder />
+</Visibility>
+
+{#if $IS_BROWSER}
+    {#await import('./providers/Debug.svelte') then { default: Debug }}
+        <Debug />
+    {/await}
+{/if}
